a09901c7bf5c1bbe657214620c29b503d5c11a16
Sn0wfaller <40021928+Sn0wfaller@users.noreply.github.com>
2022-04-21 20:52:41 +0300

    Add files via upload

---
+++ script_lab1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+loop=true
+
+while [ $loop = true ]
+do
+	echo -n "Введите имя файла: "
+	read fileName;
+
+	if [ -f $fileName ]
+	then
+		echo 'Файл уже существует, перезаписать?(y/n)'
+		read choice
+
+		if [ $choice = 'y' ]
+		then
+			uname -a > $fileName
+			loop=false
+
+		else
+			loop=true
+		fi
+	
+	else
+	uname -a > $fileName
+	loop=false
+	fi
+done
+whoami >> $fileName
+echo 'Файл успешно записан!'
